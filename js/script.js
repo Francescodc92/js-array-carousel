0fae2b7bb0b66a4e467fbce3223985f94ebfeaa3
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-06-16 17:12:47 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+console.log("hello world")
+/*
+1. Costruiamo del carosello una versione statica contenente solamente un'immagine. Di questa versione statica al momento opportuno commenteremo (oscureremo) alcuni elementi per poterli riprodurre dinamicamente in js. Potremo quindi usarli come "template".
+2. Scriviamo sempre prima per punti il nostro algoritmo in italiano per capire cosa vogliamo fare
+3. Al momento giusto (ihihhi starà a voi capire quale) rispondete a questa domanda: "Quanti cicli servono?"
+*/
